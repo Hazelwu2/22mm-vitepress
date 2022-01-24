608c90377ddfe0a9f1d65c6f9a72077d57edfaa3
Hazel Wu <tinazx056@gmail.com>
2022-01-25 01:56:43 +0800

    feat(config): setup config

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -10,11 +10,27 @@ module.exports = {
     repo: 'https://github.com/hazelwu2/22mm-vitepress',
     lastUpdated: true,
 
+    algolia: {
+      appId: process.env.ALOGOLIA_APP_ID,
+      apiKey: process.env.ALOGOLIA_API_KEY,
+      indexName: '22mm'
+    },
 
     logo: '/document.svg',
 
     sidebar: [
-
+      {
+        text: 'Hack',
+        link: '/Hack/autofill'
+      },
+      {
+        text: 'Javascript 多得是你不知道的事',
+        link: '/Javascript/destructuring-assignment'
+      },
+      {
+        text: 'Nodejs',
+        link: '/Nodejs/init'
+      },
     ]
   },
 }
